a23b89e6ae9319232df29a52e13e10fa52017550
Vivin Paliath <vivin.paliath@gmail.com>
2015-09-18 03:24:34 -0700

    Initial commit.
    enumjs - type-safe enums in JavaScript.

---
+++ README.md
@@ -0,0 +1,169 @@
+enumjs
+======
+
+This is an attempt at realizing type-safe enums in JavaScript. I'm most familiar with the way enums work in Java, and so I modeled this library after that. JavaScript doesn't have true enums. Most workarounds to this problem involve using a map where the keys represent the enum constants, and the values are integers or string-representations of the enum constants. This is a convenient solution, but the main problem is that you don't really get any type-safety since the values are just regular JavaScript types. This means that you can't even do `instanceof` checks, and you have to resort to checking the value against all defined-values to see if it is valid.
+
+I figured there must be a better way to realize enums in JavaScript while addressing these shortcomings, and this is my attempt to do that. As far as I can tell, this works like one would expect, but I'm sure there are things I haven't considered. So here's what you get with enumjs:
+
+ - The ability to define your own enum and its constants.
+ - Your custom enum is its own type, and all its constants are instances of the enum itself. This means that you *can* do `instanceof` checks.
+ - The custom enum has a `values` and a `fromName`<sup>\*</sup> method. The former returns an array of all constants defined on the enum, and the latter will attempt to return an enum consant with the same name as the string that is passed in. If one does not exist, an exception is thrown.
+ - Each enum constant has a `name()` and an `ordinal()` method. The `name()` method returns a string representing the name of the constant (as defined), and `ordinal()` returns the position of the enum (as defined).
+ - Once defined, the enum type and its constants are immutable.
+
+<sup>\*</sup>In Java the method is actually called `valueOf` and that's what I named it here originally as well. However, JavaScript has its own `valueOf` method on objects that does something else entirely, and I didn't want to override that behavior.
+
+This works on the browser and in Nashorn (the two places I tried it out). It might work on Node as well, but I haven't really tried it out. To use it, just include `enum.js`. There is a single object called `Enum`, with a method called `define`. The signature is `Enum.define(<string>, <array> | <object>)`. Some examples:
+
+```javascript
+Enum.define("Days", ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]);
+
+Days.Monday instanceof Days; // true
+
+Days.Friday.name(); // "Friday"
+Days.Friday.ordinal(); // 4
+
+Days.Sunday === Days.Sunday; // true
+Days.Sunday === Days.Friday; // false
+
+Days.Sunday.toString(); // "Sunday"
+
+Days.toString() // "Days { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday } "
+
+Days.values().map(function(e) { return e.name(); }); //["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+Days.values()[4].name(); //"Friday"
+
+Days.fromName("Thursday") === Days.Thursday // true
+Days.fromName("Wednesday").name() // "Wednesday"
+Days.Friday.fromName("Saturday").name() // "Saturday"
+```
+
+You can also attach behavior to each constant, just like in Java:
+
+```javascript
+Enum.define("Days", {
+    Monday: {
+        say: function () {
+            return this.name() + "s are bad!";
+        }
+    },
+    Tuesday: {
+        say: function () {
+            return this.name() + "s are ok...";
+        }
+    },
+    Wednesday: {
+        say: function () {
+            return this.name() + " is the middle of the week...";
+        }
+    },
+    Thursday: {
+        say: function () {
+            return this.name() + "! We're getting closer to the weekend!";
+        }
+    },
+    Friday: {
+        say: function () {
+            return this.name() + ", " + this.name() + ", Gettin' down on " + this.name() + "!";
+        }
+    },
+    Saturday: {
+        say: function () {
+            return this.name() + "! Aw yisss time for cartoons!";
+        }
+    },
+    Sunday: {
+        say: function () {
+            return this.name() + "! It's still the weekend!";
+        }
+    }
+});
+
+Days.Monday.say(); // "Mondays are bad!"
+Days.Friday.say(); // "Friday, Friday, Gettin' down on Friday!"
+```
+
+Here's another example that's based on the [Planet example](https://docs.oracle.com/javase/tutorial/java/javaOO/enum.html) from the Java documentation on enums:
+
+```javascript
+Enum.define("Planet", {
+    MERCURY: {
+        mass: 3.303e+23,
+        radius: 2.4397e6,
+        surfaceGravity: surfaceGravity,
+        surfaceWeight: surfaceWeight
+    },
+    VENUS: {
+        mass: 4.869e+24,
+        radius: 6.0518e6,
+        surfaceGravity: surfaceGravity,
+        surfaceWeight: surfaceWeight
+    },
+    EARTH: {
+        mass: 5.976e+24,
+        radius: 6.37814e6,
+        surfaceGravity: surfaceGravity,
+        surfaceWeight: surfaceWeight
+    },
+    MARS: {
+        mass: 6.421e+23,
+        radius: 3.3972e6,
+        surfaceGravity: surfaceGravity,
+        surfaceWeight: surfaceWeight
+    },
+    JUPITER: {
+        mass: 1.9e+27,
+        radius: 7.1492e7,
+        surfaceGravity: surfaceGravity,
+        surfaceWeight: surfaceWeight
+    },
+    SATURN: {
+        mass: 5.688e+26,
+        radius: 6.0268e7,
+        surfaceGravity: surfaceGravity,
+        surfaceWeight: surfaceWeight
+    },
+    URANUS: {
+        mass: 8.686e+25,
+        radius: 2.5559e7,
+        surfaceGravity: surfaceGravity,
+        surfaceWeight: surfaceWeight
+    },
+    NEPTUNE: {
+        mass: 1.024e+26,
+        radius: 2.4746e7,
+        surfaceGravity: surfaceGravity,
+        surfaceWeight: surfaceWeight
+    }
+});
+
+var G = 6.67300E-11;
+
+function surfaceGravity() {
+    return (G * this.mass) / Math.pow(this.radius, 2);
+}
+
+function surfaceWeight(mass) {
+    return mass * this.surfaceGravity();
+}
+
+var mass = 175 / Planet.EARTH.surfaceGravity();
+Planet.values().forEach(function(planet) {
+    console.log("Your weight on", planet.toString(), "is", planet.surfaceWeight(mass));
+});
+```
+
+This returns the output:
+
+```
+Your weight on MERCURY is 66.10758266016366
+Your weight on VENUS is 158.37484247218296
+Your weight on EARTH is 174.99999999999997
+Your weight on MARS is 66.27900720649754
+Your weight on JUPITER is 442.8475669617546
+Your weight on SATURN is 186.55271929202414
+Your weight on URANUS is 158.39725989314937
+Your weight on NEPTUNE is 199.20741268219012
+```
+
+That's pretty much it. Please try it out and let me know what you think, and if there are any issues, etc.
---
+++ enum.js
@@ -0,0 +1,185 @@
+/**
+ * enum.js - Type-safe enums in JavaScript. Modeled after Java enums.
+ * Version 1.0.0
+ * Written by Vivin Paliath (http://vivin.net)
+ * License: BSD License
+ * Copyright (C) 2015
+ */
+
+var Enum = (function (global) {
+    /**
+     * Function to define an enum
+     * @param typeName - The name of the enum.
+     * @param constants - The constants on the enum. Can be an array of strings, or an object where each key is an enum
+     * constant, and the values are objects that describe attributes that can be attached to the associated constant.
+     */
+    function define(typeName, constants) {
+
+        /** Check Arguments **/
+        if (typeof typeName === "undefined") {
+            throw new TypeError("A name is required.");
+        }
+
+        if (!(constants instanceof Array) && (Object.getPrototypeOf(constants) !== Object.prototype)) {
+
+            throw new TypeError("The constants parameter must either be an array or an object.");
+
+        } else if ((constants instanceof Array) && constants.length === 0) {
+
+            throw new TypeError("Need to provide at least one constant.");
+
+        } else if ((constants instanceof Array) && !constants.reduce(function (isString, element) {
+                return isString && (typeof element === "string");
+            }, true)) {
+
+            throw new TypeError("One or more elements in the constant array is not a string.");
+
+        } else if (Object.getPrototypeOf(constants) === Object.prototype && !Object.keys(constants).reduce(function (isObject, constant) {
+                return Object.getPrototypeOf(constants[constant]) === Object.prototype;
+            }, true)) {
+
+            throw new TypeError("One or more constants do not have an associated object-value.");
+
+        }
+
+        var isArray = (constants instanceof Array);
+        var isObject = !isArray;
+
+        /** Private sentinel-object used to guard enum constructor so that no one else can create enum instances **/
+        function __() { };
+
+        /** Dynamically define a function with the same name as the enum we want to define. **/
+        var __enum = new Function(["__"],
+            "return function " + typeName + "(sentinel, name, ordinal) {" +
+                "if(!(sentinel instanceof __)) {" +
+                    "throw new TypeError(\"Cannot instantiate an instance of " + typeName + ".\");" +
+                "}" +
+
+                "this.__name = name;" +
+                "this.__ordinal = ordinal;" +
+            "}"
+        )(__);
+
+        /** Private objects used to maintain enum instances for values(), and to look up enum instances for fromName() **/
+        var __values = [];
+        var __dict = {};
+
+        /** Attach values() and fromName() methods to the class itself (kind of like static methods). **/
+        Object.defineProperty(__enum, "values", {
+            value: function () {
+                return __values;
+            }
+        });
+
+        Object.defineProperty(__enum, "fromName", {
+            value: function (name) {
+                var __constant = __dict[name]
+                if (__constant) {
+                    return __constant;
+                } else {
+                    throw new TypeError(typeName + " does not have a constant with name " + name + ".");
+                }
+            }
+        });
+
+        /**
+         * The following methods are available to all instances of the enum. values() and fromName() need to be
+         * available to each constant, and so we will attach them on the prototype. But really, they're just
+         * aliases to their counterparts on the prototype.
+         */
+        Object.defineProperty(__enum.prototype, "values", {
+            value: __enum.values
+        });
+
+        Object.defineProperty(__enum.prototype, "fromName", {
+            value: __enum.fromName
+        });
+
+        Object.defineProperty(__enum.prototype, "name", {
+            value: function () {
+                return this.__name;
+            }
+        });
+
+        Object.defineProperty(__enum.prototype, "ordinal", {
+            value: function () {
+                return this.__ordinal;
+            }
+        });
+
+        Object.defineProperty(__enum.prototype, "valueOf", {
+            value: function () {
+                return this.__name;
+            }
+        });
+
+        Object.defineProperty(__enum.prototype, "toString", {
+            value: function () {
+                return this.__name;
+            }
+        });
+
+        /**
+         * If constants was an array, we can the element values directly. Otherwise, we will have to use the keys
+         * from the constants object.
+         */
+        var _constants = constants;
+        if (isObject) {
+            _constants = Object.keys(constants);
+        }
+
+        /** Iterate over all constants, create an instance of our enum for each one, and attach it to the enum type **/
+        _constants.forEach(function (name, ordinal) {
+            // Create an instance of the enum
+            var __constant = new __enum(new __(), name, ordinal);
+
+            // If constants was an object, we want to attach the provided attributes to the instance.
+            if (isObject) {
+                Object.keys(constants[name]).forEach(function (attr) {
+                    Object.defineProperty(__constant, attr, {
+                        value: constants[name][attr]
+                    });
+                });
+            }
+
+            // Freeze the instance so that it cannot be modified.
+            Object.freeze(__constant);
+
+            // Attach the instance using the provided name to the enum type itself.
+            Object.defineProperty(__enum, name, {
+                value: __constant
+            });
+
+            // Update our private objects
+            __values.push(__constant);
+            __dict[name] = __constant;
+        });
+
+        /** Define a friendly toString method for the enum **/
+        var string = typeName + " { " + __enum.values().map(function (c) {
+                return c.name();
+            }).join(", ") + " } ";
+
+        Object.defineProperty(__enum, "toString", {
+            value: function () {
+                return string;
+            }
+        });
+
+        /** Freeze our private objects **/
+        Object.freeze(__values);
+        Object.freeze(__dict);
+
+        /** Freeze the prototype on the enum and the enum itself **/
+        Object.freeze(__enum.prototype);
+        Object.freeze(__enum);
+
+        /** Add the enum to the global scope **/
+        global[typeName] = __enum;
+    }
+
+    return {
+        define: define
+    }
+
+})(typeof window !== "undefined" ? window : this);
